8f419fa00d3cc3f2f1afc7e4c1fb84416ce6db32
Joel L <floptical.bob@gmail.com>
2014-06-21 16:18:33 -0400

    Getting closer to a legitimate configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,17 +3,32 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    serve: {
+    connect: {
+      server: {
         options: {
-            port: 9000
+          port: 9000,
+          hostname: '*'
         }
+      }
+    },
+    open: {
+      server: {
+        url: 'http://localhost:<%= connect.server.options.port %>/index.html',
+        app: 'chrome'
+      }
     }
   });
 
-  // Load grunt-serve
-  grunt.loadNpmTasks('grunt-serve');
+  grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('serve', function (target) {
+    grunt.task.run([
+      'connect',
+      'open'
+    ]);
+  });
 
   // Default task(s).
   grunt.registerTask('default', ['serve']);
-
 };
