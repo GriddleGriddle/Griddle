5ce3a37ff5373daae8563ae628cc17bdb87eab99
Joel L <floptical.bob@gmail.com>
2014-06-21 15:51:08 -0400

    Typo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,12 @@
-
-
-
-
-grunt.initConfig({
-    serve: {
-        options: {
-            port: 9000
-        }
-    }
-});
-
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
+    serve: {
+        options: {
+            port: 9000
+        }
     }
   });
 
@@ -30,6 +14,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-serve');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['serve']);
 
 };
