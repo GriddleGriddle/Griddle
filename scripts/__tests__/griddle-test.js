4678a3c6a0000351bae4c6c71267813fdd845344
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2014-08-24 21:21:16 -0400

    Working test against griddle. Not a very good test but a test.

--- scripts/__tests__/griddle-test.js
+++ scripts/__tests__/griddle-test.js
@@ -1,12 +1,5 @@
 /** @jsx React.DOM */
-/*jest.dontMock('../griddle.jsx');
-jest.dontMock('../gridBody.jsx');
-jest.dontMock('../gridFilter.jsx');
-jest.dontMock('../gridPagination.jsx');
-jest.dontMock('../gridRow.jsx');
-jest.dontMock('../gridRowContainer.jsx');*/
 jest.dontMock('../griddle.jsx');
-//jest.dontMock('../gridTitle.jsx');
 
 var React = require('react/addons');
 var Griddle = require('../griddle.jsx');
@@ -35,10 +28,22 @@ describe('Griddle', function() {
       }
     ];
 
-    var grid = TestUtils.renderIntoDocument(
-     <Griddle results={fakeData}/>
-    );
+    var grid = <Griddle results={fakeData} />
+    grid = TestUtils.renderIntoDocument(grid);
 
-//    expect(TestUtils.isComponentOfType(griddle, Griddle)).toBe(true);
+    expect(grid == null).toBe(false);
+    expect(grid.state).toEqual({
+      maxPage: 1,
+      page: 0,
+      filteredResults: null,
+      filteredColumns: [],
+      filter: "",
+      sortColumn: "",
+      sortAscending: true,
+      showColumnChooser: false,
+      isLoading: false,
+      results: fakeData
+    }); 
+
   });
 });
