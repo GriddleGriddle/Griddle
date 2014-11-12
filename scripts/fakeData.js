664aaf39341d3e6fc1ca4cca167069029c9ea2d7
Joel L <floptical.bob@gmail.com>
2014-11-12 12:40:24 -0500

    Updated getExternalResults documentation to be a little more accurate

--- scripts/fakeData.js
+++ scripts/fakeData.js
@@ -56,7 +56,7 @@ var propertiesItem = [
   },
   {
     "property": "getExternalResults",
-    "description": "A function that obtains data to display in the grid and returns the results, along with the max total number of results. The methods requires the following parameters",
+    "description": "A function that obtains data to display in the grid and passes the results into a callback parameter, along with the max total number of results. The methods requires the following parameters",
     "children": [{
         "property": "filterString",
         "description": "The string that the user is filtering by."
@@ -79,7 +79,7 @@ var propertiesItem = [
       },
       {
         "property": "callback",
-        "description": "The function that the results will be passed into. <br /> The callback needs the following properties",
+        "description": "The function that the results will be passed into.  The callback needs the following properties",
         "children":[
           {
             "property": "results",
