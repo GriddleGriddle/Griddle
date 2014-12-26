cf3ce60ca712eba65732056b45d082414d65f6ad
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2014-12-26 16:11:08 -0500

    Adds example grid to landing page. Needs work.

--- examples/assets/scripts/fakeData.js
+++ examples/assets/scripts/fakeData.js
@@ -3,6 +3,51 @@
 
 var emptyData = [];
 
+var columnMeta = [
+  {
+    "columnName": "id",
+    "order": 1,
+    "locked": false,
+    "visible": true
+  },
+  {
+    "columnName": "name",
+    "order": 2,
+    "locked": false,
+    "visible": true
+  },
+  {
+    "columnName": "city",
+    "order": 3,
+    "locked": false,
+    "visible": true
+  },
+  {
+    "columnName": "state",
+    "order": 4,
+    "locked": false,
+    "visible": true
+  },
+  {
+    "columnName": "country",
+    "order": 5,
+    "locked": false,
+    "visible": true
+  },
+  {
+    "columnName": "company",
+    "order": 6,
+    "locked": false,
+    "visible": true
+  },
+  {
+    "columnName": "favoriteNumber",
+    "order":  7,
+    "locked": false,
+    "visible": true
+  }
+];
+
 var propertyGridMeta = [
   {
     "columnName": "property",
--- examples/assets/scripts/testComponent.js
+++ examples/assets/scripts/testComponent.js
@@ -115,50 +115,6 @@ var ExternalComponent = React.createClass({
     }
 });
 
-var columnMeta = [
-  {
-    "columnName": "id",
-    "order": 1,
-    "locked": false,
-    "visible": true
-  },
-  {
-    "columnName": "name",
-    "order": 2,
-    "locked": false,
-    "visible": true
-  },
-  {
-    "columnName": "city",
-    "order": 3,
-    "locked": false,
-    "visible": true
-  },
-  {
-    "columnName": "state",
-    "order": 4,
-    "locked": false,
-    "visible": true
-  },
-  {
-    "columnName": "country",
-    "order": 5,
-    "locked": false,
-    "visible": true
-  },
-  {
-    "columnName": "company",
-    "order": 6,
-    "locked": false,
-    "visible": true
-  },
-  {
-    "columnName": "favoriteNumber",
-    "order":  7,
-    "locked": false,
-    "visible": true
-  }
-];
 
 var TestComponent = React.createClass({
     getDefaultProps: function() {
