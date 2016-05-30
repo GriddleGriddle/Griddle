4528c5e0905c0e9b3a83a5d9acc1b163884a8c2f
odk211 <tomoki.odaka@gmail.com>
2016-05-31 01:15:30 +0900

    fix gridRow defaultValue logic

--- scripts/gridRow.jsx
+++ scripts/gridRow.jsx
@@ -78,7 +78,7 @@ var GridRow = React.createClass({
 
         // make sure that all the columns we need have default empty values
         // otherwise they will get clipped
-        var defaultValues = fromPairs(columns, []);
+        var defaultValues = fromPairs(columns.map((col) => [col, null]));
 
         // creates a 'view' on top the data so we will not alter the original data but will allow us to add default values to missing columns
         var dataView = assign({}, this.props.data);
