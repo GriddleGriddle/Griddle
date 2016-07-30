9d1d834eafa8f26d41a20d8bafa38112c00aac79
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2016-07-30 14:13:04 -0400

    Add some composition methods

--- src/utils/__tests__/compositionUtilsTest.js
+++ src/utils/__tests__/compositionUtilsTest.js
@@ -1,5 +1,10 @@
 import test from 'ava';
-import { extendArray } from '../compositionUtils';
+import {
+  extendArray,
+  getPropertiesByEnding,
+  getObjectWherePropertyEndsWith,
+  composeReducer
+} from '../compositionUtils';
 
 test('combine works', test => {
   const itemOne = { one: 'one', three: 'three' };
@@ -11,3 +16,68 @@ test('combine works', test => {
 
   test.deepEqual(combined, { one: 'one', two: 'two', three: 'san', four: 'four' })
 });
+
+test('gets properties by ending', test => {
+  const object = {
+    one: 1,
+    twoTEST: 2,
+    three: 3,
+    four: 4,
+    fiveTEST: 5,
+    six: 6
+  };
+
+  const filteredProperties = getPropertiesByEnding('TEST', object);
+  test.deepEqual(filteredProperties, ['twoTEST', 'fiveTEST']);
+});
+
+test('gets object by property ending', test => {
+  const object = {
+    one: 1,
+    twoTEST: 2,
+    three: 3,
+    four: 4,
+    fiveTEST: 5,
+    six: 6
+  };
+
+  const objectByEnding = getObjectWherePropertyEndsWith('TEST', object);
+  test.deepEqual(objectByEnding, { twoTEST: 2, fiveTEST: 5});
+})
+
+test('gets a wrapped reducer in the correct order', test => {
+  const reducer1 = function(state, action) {
+    return { number: state.number + 5 };
+  }
+
+  const reducer2 = function(state, action) {
+    return { number: state.number * 10 };
+  }
+
+  const composed1 = composeReducer(reducer1, reducer2);
+  test.deepEqual(composed1({ number: 5 }), { number: 100 });
+
+  const composed2 = composeReducer(reducer2, reducer1);
+  test.deepEqual(composed2({ number: 5}), { number: 55 });
+});
+
+test('it has the correct action data in composed methods', test => {
+  let action1, action2 = null;
+
+  const reducer1 = function(state, action) {
+    action1 = action;
+    return state;
+  }
+
+  const reducer2 = function(state, action) {
+    action2 = action;
+    return state;
+  }
+
+  const composed = composeReducer(reducer1, reducer2)({ number: 5}, { type: 'Yo!' });
+  test.not(action1, null);
+  test.is(action1, action2);
+  test.deepEqual(action1, { type: 'Yo!' });
+});
+
+
--- src/utils/compositionUtils.js
+++ src/utils/compositionUtils.js
@@ -18,3 +18,77 @@ export function extendArray(objects) {
   //return the combined object
   return combinedObject;
 }
+
+//from MDN
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+      var subjectString = this.toString();
+      if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+        position = subjectString.length;
+      }
+      position -= searchString.length;
+      var lastIndex = subjectString.indexOf(searchString, position);
+      return lastIndex !== -1 && lastIndex === position;
+  };
+}
+
+/**
+ * Finds properties on an object that end in specified word
+ * @param {string} ending - The string that properties should be found ending with
+ * @param {Object} object - The object to find keys on
+ */
+export function getPropertiesByEnding(ending, object) {
+  return Object.keys(object).filter((name) => name.endsWith(ending));
+}
+
+/** Creates a new object containing only properties that end with specified ending
+ * @param {string} ending - The string that properties should be found ending with
+ * @param {Object} object - The object to find keys on
+ */
+export function getObjectWherePropertyEndsWith(ending, object) {
+  const keys = getPropertiesByEnding(ending, object);
+
+  return _.pick(object, keys);
+}
+
+/** Creates a new reducer by taking the output of the first reducer as state to the second
+ * @param {Object} currentReducer - reducerMethod (state, action) to that we want to run as the state parameter for second reducer
+ * @param {Object} previousReducer - reducerMethod (state, action) to run second
+ */
+export function composeReducer(nextReducer, previousReducer) {
+  // compose the reducers when both parameters are functions
+  if(typeof(nextReducer) === 'function' && typeof(previousReducer) === 'function') {
+    return (state, action) => previousReducer(nextReducer(state, action), action);
+  }
+
+  // return the nextReducer
+  return nextReducer;
+}
+
+export function getReducersByWordEnding(reducers, ending) {
+  return reducers.reduce((previous, current) => {
+    const keys = Object.keys(current).filter((name) => name.endsWith(ending));
+
+    let reducer = pick(current, keys);
+
+    //TODO: clean this up it's a bit hacky
+    for (var key in current) {
+      if(!key.endsWith(ending)) { continue; }
+
+      const keyWithoutEnding = key.replace(`_${ending}`, "");
+      //make a new method that pipes output of previous into state of current
+      //this is to allow chaining these
+      const hasPrevious =  previous.hasOwnProperty(keyWithoutEnding) && typeof previous[keyWithoutEnding] === 'function';
+      const previousReducer = hasPrevious ?
+        previous[keyWithoutEnding] :
+        undefined;
+
+      const currentReducer = reducer[key]
+
+      reducer[keyWithoutEnding] = wrapReducer(currentReducer, previousReducer);
+    }
+
+    //override anything in previous (since this now calls previous to make sure we have helpers from both);
+    return extend(previous, reducer);
+  }, {});
+}
