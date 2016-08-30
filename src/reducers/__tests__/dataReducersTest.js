f6a8534ccff42a1759322bba87a393aa5a55bfa7
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2016-08-30 01:18:01 -0400

    Update dataReducer tests

--- src/reducers/__tests__/dataReducersTest.js
+++ src/reducers/__tests__/dataReducersTest.js
@@ -4,6 +4,21 @@ import Immutable from 'immutable';
 import * as reducers from '../dataReducers';
 import constants from '../../constants';
 
+test('initializes data', test => {
+  const initializedState = reducers.GRIDDLE_INITIALIZED(new Immutable.Map(),
+    { type: 'GRIDDLE_INITIALIZED',
+      properties: {
+        one: 'one',
+        two: 'two'
+      }
+  });
+
+  test.deepEqual(initializedState.get('renderProperties').toJSON(), {
+    one: 'one',
+    two: 'two'
+  });
+});
+
 test('sets data', test => {
   const reducedState = reducers.GRIDDLE_LOADED_DATA(new Immutable.Map(),
     { type: 'GRIDDLE_LOADED_DATA', data: [
@@ -12,10 +27,11 @@ test('sets data', test => {
     ]}
   );
 
-  test.deepEqual(reducedState.toJSON(), { data:
-    [
-      {name: "one"},
-      {name: "two"}
-    ]}
-  )
-})
+  test.deepEqual(reducedState.toJSON(), {
+    data: [
+      {name: "one", griddleKey: 0},
+      {name: "two", griddleKey: 1}
+    ],
+    loading: false
+  });
+});
--- src/reducers/dataReducers.js
+++ src/reducers/dataReducers.js
@@ -1,5 +1,23 @@
 import Immutable from 'immutable';
 
-export const GRIDDLE_LOADED_DATA = (state, action) => (
-  state.set('data', action.data)
-);
+import {
+  addKeyToCollection
+} from '../utils/dataUtils';
+
+/** Sets the default render properties
+ * @param {Immutable} state- Immutable previous state object
+ * @param {Object} action - The action object to work with
+*/
+//TODO: Consider renaming this to be more in line with what it's actually doing
+export function GRIDDLE_INITIALIZED(state, action) {
+  return state.set('renderProperties', Immutable.fromJS(action.properties));
+}
+
+/** Sets the griddle data
+ * @param {Immutable} state- Immutable previous state object
+ * @param {Object} action - The action object to work with
+*/
+export function GRIDDLE_LOADED_DATA(state, action, helpers) {
+  return state.set('data', addKeyToCollection(Immutable.fromJS(action.data)))
+    .set('loading', false);
+}
