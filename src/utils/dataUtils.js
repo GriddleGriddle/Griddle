672b65d4f9af34fa396fc63c311bcbfd1cc4bce8
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2016-08-30 01:09:58 -0400

    Add docs to addKeyToCollection

--- src/utils/dataUtils.js
+++ src/utils/dataUtils.js
@@ -1,3 +1,7 @@
+/** adds griddleKey to given collection
+ * @param (List<Map>) data - data collection to work against
+ * @param (object) settings - settings object -- default {}
+ */
 export function addKeyToCollection(data, settings={}) {
   const defaultSettings = { name: 'griddleKey', startIndex: 0 };
   const localSettings = Object.assign({}, defaultSettings, settings);
