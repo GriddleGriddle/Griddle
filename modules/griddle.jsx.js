6945ced276104958221bff1dd7284782263aac6f
John Blossom <jblossom@medianewsgroup.com>
2016-10-25 11:41:52 -0600

    feature request: allow a custom componentDidMount() method to be passed

--- modules/griddle.jsx.js
+++ modules/griddle.jsx.js
@@ -463,6 +463,11 @@ var Griddle = React.createClass({
             });
         }
     },
+    componentDidMount: function componentDidMount() {
+        if(this.props.componentDidMount && typeof this.props.componentDidMount === "function") {
+            return this.props.componentDidMount();
+        }
+    },
     //todo: clean these verify methods up
     verifyExternal: function verifyExternal() {
         if (this.props.useExternal === true) {
