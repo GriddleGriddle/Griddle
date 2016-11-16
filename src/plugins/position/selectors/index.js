5695fb2fde82b7e60c647ab60ef7d59f23ba48b3
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2016-11-16 07:11:33 -0500

    Add comment about how to proceed

--- src/plugins/position/selectors/index.js
+++ src/plugins/position/selectors/index.js
@@ -17,3 +17,9 @@ export const visibleRecordCountSelector = createSelector(
     return Math.ceil(height / rowHeight);
   }
 );
+
+// From what i can tell from the original virtual scrolling plugin...
+// 1. We want to get the visible record count
+// 2. Get the size of the dataset we're working with (whether thats local or remote)
+// 3. Figure out the renderedStart and End display index
+// 4. Show only the records that'd fall in the render indexes
