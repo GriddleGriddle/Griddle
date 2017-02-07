89fc98c09c955fa3b557bd653adf107f3483a702
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2017-02-07 06:54:24 -0500

    Settings tests

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
+// This is responsible for rendering the individual settings sections
 const Settings = ({ settingsComponents, style, className }) => (
   <div style={style} className={className}>
-    {settingsComponents.map((SettingsComponent) => <div><SettingsComponent /></div>)}
+    {settingsComponents && settingsComponents.map((SettingsComponent) => <div><SettingsComponent /></div>)}
   </div>
 )
 
