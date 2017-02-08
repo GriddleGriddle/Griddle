0818d77b59b3d5feae631e289e095d50bd8a833d
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2017-02-08 05:27:23 -0500

    Add  settings wrapper tests

--- src/components/SettingsWrapper.js
+++ src/components/SettingsWrapper.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+// This is a component that wraps all of the other settings components ( SettingsToggle, Settings, etc).
+// All of the settings views will be hiddne if isEnabled = false
 const SettingsWrapper = ({ SettingsToggle, Settings, isEnabled, isVisible, style, className }) => (
   isEnabled ? (
     <div style={style} className={className}>
-      <SettingsToggle />
+      { SettingsToggle && <SettingsToggle /> }
       { isVisible && <Settings /> }
     </div>
   ) : null
