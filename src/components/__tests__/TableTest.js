3c214adb30357c07f41b6653b2b13cb5e213ac1c
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2017-02-18 06:53:47 -0500

    Fix tests

--- src/components/__tests__/TableTest.js
+++ src/components/__tests__/TableTest.js
@@ -5,34 +5,41 @@ import { shallow } from 'enzyme';
 import Table from '../Table';
 
 test('renders', (t) => {
-  const wrapper = shallow(<Table />);
+  const wrapper = shallow(<Table visibleRows={5} />);
 
   t.true(wrapper.matchesElement(<table />));
 });
 
 test('renders with style', (t) => {
   const style = { backgroundColor: '#EDEDED' };
-  const wrapper = shallow(<Table style={style} />);
+  const wrapper = shallow(<Table style={style} visibleRows={5} />);
 
   t.true(wrapper.matchesElement(<table style={{ backgroundColor: '#EDEDED' }} />));
 });
 
 test('renders with className', (t) => {
-  const wrapper = shallow(<Table className="className" />);
+  const wrapper = shallow(<Table className="className" visibleRows={5} />);
 
   t.true(wrapper.matchesElement(<table className="className" />));
 });
 
 test('renders TableHeading', (t) => {
   const TableHeading = () => <thead />;
-  const wrapper = shallow(<Table TableHeading={TableHeading} />);
+  const wrapper = shallow(<Table TableHeading={TableHeading} visibleRows={5} />);
 
   t.true(wrapper.matchesElement(<table><TableHeading /></table>));
 });
 
-test('redners TableBody', (t) => {
+test('renders TableBody', (t) => {
   const TableBody = () => <tbody />;
-  const wrapper = shallow(<Table TableBody={TableBody} />);
+  const wrapper = shallow(<Table TableBody={TableBody} visibleRows={5} />);
 
   t.true(wrapper.matchesElement(<table><TableBody /></table>));
+});
+
+test('renders NoResults when noRows', (t) => {
+  const NoResults = () => <marquee />;
+  const wrapper = shallow(<Table NoResults={NoResults} visibleRows={0} />);
+
+  t.true(wrapper.matchesElement(<NoResults />));
 });
