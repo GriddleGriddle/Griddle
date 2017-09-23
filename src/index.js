82bde75eb3d1ddb64c526ff6b59ea150f4ba97a6
Keith Dahlby <dahlbyk@gmail.com>
2017-09-23 15:32:19 -0500

    Extract initializer util from Griddle constructor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { createStore, combineReducers, bindActionCreators, applyMiddleware, compose } from 'redux';
-import Immutable from 'immutable';
 import { createProvider } from 'react-redux';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
@@ -10,9 +9,7 @@ import components from './components';
 import settingsComponentObjects from './settingsComponentObjects';
 import * as selectors from './selectors/dataSelectors';
 
-import { buildGriddleReducer, buildGriddleComponents } from './utils/compositionUtils';
-import { getColumnProperties } from './utils/columnUtils';
-import { getRowProperties } from './utils/rowUtils';
+import init from './utils/initializer';
 import { setSortProperties } from './utils/sortUtils';
 import { StoreListener } from './utils/listenerUtils';
 import * as actions from './actions';
@@ -69,55 +66,20 @@ class Griddle extends Component {
     super(props);
 
     const {
-      plugins=[],
-      data,
-      children:rowPropertiesComponent,
-      events={},
-      sortProperties={},
-      styleConfig={},
-      pageProperties:importedPageProperties,
-      components:userComponents,
-      renderProperties:userRenderProperties={},
-      settingsComponentObjects:userSettingsComponentObjects,
       storeKey = Griddle.storeKey || 'store',
-      reduxMiddleware = [],
-      listeners = {},
-      ...userInitialState
     } = props;
 
-    const rowProperties = getRowProperties(rowPropertiesComponent);
-    const columnProperties = getColumnProperties(rowPropertiesComponent);
-
-    //Combine / compose the reducers to make a single, unified reducer
-    const reducers = buildGriddleReducer([dataReducers, ...plugins.map(p => p.reducer)]);
-
-    //Combine / Compose the components to make a single component for each component type
-    this.components = buildGriddleComponents([components, ...plugins.map(p => p.components), userComponents]);
-
-    this.settingsComponentObjects = Object.assign({}, settingsComponentObjects, ...plugins.map(p => p.settingsComponentObjects), userSettingsComponentObjects);
-
-    this.events = Object.assign({}, events, ...plugins.map(p => p.events));
-
-    this.selectors = plugins.reduce((combined, plugin) => ({ ...combined, ...plugin.selectors }), {...selectors});
-
-    const mergedStyleConfig = _.merge({}, defaultStyleConfig, ...plugins.map(p => p.styleConfig), styleConfig);
-
-    const pageProperties = Object.assign({}, {
+    const { initialState, reducers, reduxMiddleware } = init.call(this, {
+      reducers: dataReducers,
+      components,
+      settingsComponentObjects,
+      selectors,
+      styleConfig: defaultStyleConfig,
+      pageProperties: {
         currentPage: 1,
         pageSize: 10
       },
-      importedPageProperties,
-    );
-
-    //TODO: This should also look at the default and plugin initial state objects
-    const renderProperties = Object.assign({
-      rowProperties,
-      columnProperties
-    }, ...plugins.map(p => p.renderProperties), userRenderProperties);
-
-    // TODO: Make this its own method
-    const initialState = _.merge(
-      {
+      initialState: {
         enableSettings: true,
         textProperties: {
           next: 'Next',
@@ -125,30 +87,19 @@ class Griddle extends Component {
           settingsToggle: 'Settings'
         },
       },
-      ...plugins.map(p => p.initialState),
-      userInitialState,
-      {
-        data,
-        pageProperties,
-        renderProperties,
-        sortProperties,
-        styleConfig: mergedStyleConfig,
-      }
-    );
+    });
 
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
     this.store = createStore(
       reducers,
       initialState,
       composeEnhancers(
-        applyMiddleware(..._.compact(_.flatten(plugins.map(p => p.reduxMiddleware))), ...reduxMiddleware)
+        applyMiddleware(...reduxMiddleware)
       )
     );
 
     this.provider = createProvider(storeKey);
 
-    const sanitizedListeners = _.pickBy(listeners, (value, key) => typeof value === "function");
-    this.listeners = plugins.reduce((combined, plugin) => ({...combined, ..._.pickBy(plugin.listeners, (value, key) => typeof value === "function")}), {...sanitizedListeners});
     this.storeListener = new StoreListener(this.store);
     _.forIn(this.listeners, (listener, name) => {
       this.storeListener.addListener(listener, name, {events: this.events, selectors: this.selectors});
---
+++ src/utils/initializer.js
@@ -0,0 +1,97 @@
+import _ from 'lodash';
+import { buildGriddleReducer, buildGriddleComponents } from './compositionUtils';
+import { getColumnProperties } from './columnUtils';
+import { getRowProperties } from './rowUtils';
+
+module.exports = function initializer(defaults) {
+  if (!this) throw new Error('this missing!');
+
+  const {
+    reducers: dataReducers,
+    components,
+    settingsComponentObjects,
+    selectors,
+    styleConfig: defaultStyleConfig,
+    pageProperties: defaultPageProperties,
+    initialState: defaultInitialState,
+  } = defaults;
+
+  const {
+    plugins = [],
+    data,
+    children: rowPropertiesComponent,
+    events: userEvents = {},
+    sortProperties = {},
+    styleConfig: userStyleConfig = {},
+    pageProperties: userPageProperties,
+    components: userComponents,
+    renderProperties: userRenderProperties = {},
+    settingsComponentObjects: userSettingsComponentObjects,
+    reduxMiddleware = [],
+    listeners = {},
+    ...userInitialState
+  } = this.props;
+
+  const rowProperties = getRowProperties(rowPropertiesComponent);
+  const columnProperties = getColumnProperties(rowPropertiesComponent);
+
+  // Combine / compose the reducers to make a single, unified reducer
+  const reducers = buildGriddleReducer([dataReducers, ...plugins.map(p => p.reducer)]);
+
+  // Combine / Compose the components to make a single component for each component type
+  this.components = buildGriddleComponents([
+    components,
+    ...plugins.map(p => p.components),
+    userComponents,
+  ]);
+
+  this.settingsComponentObjects = Object.assign(
+    { ...settingsComponentObjects },
+    ...plugins.map(p => p.settingsComponentObjects),
+    userSettingsComponentObjects);
+
+  this.events = Object.assign({}, userEvents, ...plugins.map(p => p.events));
+
+  this.selectors = plugins.reduce(
+    (combined, plugin) => ({ ...combined, ...plugin.selectors }),
+    { ...selectors });
+
+  const styleConfig = _.merge(
+    { ...defaultStyleConfig },
+    ...plugins.map(p => p.styleConfig),
+    userStyleConfig);
+
+  const pageProperties = Object.assign({}, defaultPageProperties, userPageProperties);
+
+  // TODO: This should also look at the default and plugin initial state objects
+  const renderProperties = Object.assign({
+    rowProperties,
+    columnProperties
+  }, ...plugins.map(p => p.renderProperties), userRenderProperties);
+
+  // TODO: Make this its own method
+  const initialState = _.merge(
+    defaultInitialState,
+    ...plugins.map(p => p.initialState),
+    userInitialState,
+    {
+      data,
+      pageProperties,
+      renderProperties,
+      sortProperties,
+      styleConfig,
+    }
+  );
+
+  const sanitizedListeners = _.pickBy(listeners, value => typeof value === 'function');
+  this.listeners = plugins.reduce((combined, plugin) => ({ ...combined, ..._.pickBy(plugin.listeners, value => typeof value === 'function') }), sanitizedListeners);
+
+  return {
+    initialState,
+    reducers,
+    reduxMiddleware: [
+      ..._.compact(_.flatten(plugins.map(p => p.reduxMiddleware))),
+      ...reduxMiddleware
+    ],
+  };
+};
