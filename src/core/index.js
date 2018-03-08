63adcc604a634af141cfc8732fbc440051802853
Keith Dahlby <dahlbyk@gmail.com>
2018-03-08 17:24:53 -0600

    Include settingsComponentObjects in CorePlugin
    Accidentally omitted in 3577f8fbeaf480f316e29e0f701acfcf108637c7

--- src/core/index.js
+++ src/core/index.js
@@ -2,10 +2,12 @@ import components from '../components';
 import * as reducer from '../reducers/dataReducer';
 import * as selectors from '../selectors/dataSelectors';
 import * as actions from '../actions';
+import settingsComponentObjects from '../settingsComponentObjects';
 import initialState from './initialState';
 
 const CorePlugin = {
   components,
+  settingsComponentObjects,
   reducer,
   selectors,
   actions,
