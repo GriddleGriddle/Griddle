3055c0ad43bc9b6ff06536bdb63b0b38b585ee84
Debananda <todebananda@hotmail.com>
2019-02-24 21:37:57 +0530

    Updated everything except redux and ava

--- src/utils/griddleConnect.js
+++ src/utils/griddleConnect.js
@@ -2,10 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-
 /// This method appends options onto existing connect parameters
-export const mergeConnectParametersWithOptions = (originalConnect, newOptions) => {
-  const [mapStateFromProps, mapDispatchFromProps, mergeProps, options] = originalConnect;
+export const mergeConnectParametersWithOptions = (
+  originalConnect,
+  newOptions
+) => {
+  const [
+    mapStateFromProps,
+    mapDispatchFromProps,
+    mergeProps,
+    options
+  ] = originalConnect;
 
   return [
     mapStateFromProps,
@@ -13,22 +20,25 @@ export const mergeConnectParametersWithOptions = (originalConnect, newOptions) =
     mergeProps,
     { ...options, ...newOptions }
   ];
-}
+};
 
-const griddleConnect = (...connectOptions) => OriginalComponent => class extends React.Component {
-  static contextTypes = {
-    storeKey: PropTypes.string,
-  }
+const griddleConnect = (...connectOptions) => OriginalComponent =>
+  class extends React.Component {
+    static contextTypes = {
+      storeKey: PropTypes.string
+    };
 
-  constructor(props, context) {
-    super(props, context);
-    const newOptions = mergeConnectParametersWithOptions(connectOptions, { storeKey: context.storeKey })
-    this.ConnectedComponent = connect(...newOptions)(OriginalComponent);
-  }
+    constructor(props, context) {
+      super(props, context);
+      const newOptions = mergeConnectParametersWithOptions(connectOptions, {
+        storeKey: context.storeKey
+      });
+      this.ConnectedComponent = connect(...newOptions)(OriginalComponent);
+    }
 
-  render() {
-    return <this.ConnectedComponent {...this.props}/>
-  }
-}
+    render() {
+      return <this.ConnectedComponent {...this.props} />;
+    }
+  };
 
-export { griddleConnect as connect }
+export { griddleConnect as connect };
--- src/utils/initializer.js
+++ src/utils/initializer.js
@@ -1,9 +1,15 @@
-import _ from 'lodash';
-import { buildGriddleReducer, buildGriddleComponents } from './compositionUtils';
+import merge from 'lodash/merge';
+import pickBy from 'lodash/pickBy';
+import compact from 'lodash/compact';
+import flatten from 'lodash/flatten';
+import {
+  buildGriddleReducer,
+  buildGriddleComponents
+} from './compositionUtils';
 import { getColumnProperties } from './columnUtils';
 import { getRowProperties } from './rowUtils';
 
-module.exports = function initializer(defaults) {
+function initializer(defaults) {
   if (!this) throw new Error('this missing!');
 
   const {
@@ -33,59 +39,79 @@ module.exports = function initializer(defaults) {
   const columnProperties = getColumnProperties(rowPropertiesComponent);
 
   // Combine / compose the reducers to make a single, unified reducer
-  const reducer = buildGriddleReducer([defaultReducer, ...plugins.map(p => p.reducer)]);
+  const reducer = buildGriddleReducer([
+    defaultReducer,
+    ...plugins.map(p => p.reducer)
+  ]);
 
   // Combine / Compose the components to make a single component for each component type
   this.components = buildGriddleComponents([
     components,
     ...plugins.map(p => p.components),
-    userComponents,
+    userComponents
   ]);
 
   this.settingsComponentObjects = Object.assign(
     { ...settingsComponentObjects },
     ...plugins.map(p => p.settingsComponentObjects),
-    userSettingsComponentObjects);
+    userSettingsComponentObjects
+  );
 
   this.events = Object.assign({}, userEvents, ...plugins.map(p => p.events));
 
   this.selectors = plugins.reduce(
     (combined, plugin) => ({ ...combined, ...plugin.selectors }),
-    { ...selectors });
+    { ...selectors }
+  );
 
-  const styleConfig = _.merge(
+  const styleConfig = merge(
     { ...defaultStyleConfig },
     ...plugins.map(p => p.styleConfig),
-    userStyleConfig);
-
+    userStyleConfig
+  );
 
   // TODO: This should also look at the default and plugin initial state objects
-  const renderProperties = Object.assign({
-    rowProperties,
-    columnProperties
-  }, ...plugins.map(p => p.renderProperties), userRenderProperties);
+  const renderProperties = Object.assign(
+    {
+      rowProperties,
+      columnProperties
+    },
+    ...plugins.map(p => p.renderProperties),
+    userRenderProperties
+  );
 
   // TODO: Make this its own method
-  const initialState = _.merge(
+  const initialState = merge(
     defaultInitialState,
     ...plugins.map(p => p.initialState),
     userInitialState,
     {
       data,
       renderProperties,
-      styleConfig,
+      styleConfig
     }
   );
 
-  const sanitizedListeners = _.pickBy(listeners, value => typeof value === 'function');
-  this.listeners = plugins.reduce((combined, plugin) => ({ ...combined, ..._.pickBy(plugin.listeners, value => typeof value === 'function') }), sanitizedListeners);
+  const sanitizedListeners = pickBy(
+    listeners,
+    value => typeof value === 'function'
+  );
+  this.listeners = plugins.reduce(
+    (combined, plugin) => ({
+      ...combined,
+      ...pickBy(plugin.listeners, value => typeof value === 'function')
+    }),
+    sanitizedListeners
+  );
 
   return {
     initialState,
     reducer,
-    reduxMiddleware: _.compact([
-      ..._.flatten(plugins.map(p => p.reduxMiddleware)),
+    reduxMiddleware: compact([
+      ...flatten(plugins.map(p => p.reduxMiddleware)),
       ...reduxMiddleware
-    ]),
+    ])
   };
-};
+}
+
+export default initializer;
