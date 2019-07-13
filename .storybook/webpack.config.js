58bf2bd889e1de9ec588bf4e54df7b7b11937c86
Debananda Mohanty <todebananda@hotmail.com>
2019-07-13 07:17:35 +0530

    packages updated

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-module.exports = (baseConfig, env, config) => {
+module.exports = ({ baseConfig, env, config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
